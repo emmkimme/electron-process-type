0b7d3cc71d2832cbe60582fe6a4f9c40244dcec0
emmanuel.kimmerlin@thomsonreuters.com <emmanuel.kimmerlin@thomsonreuters.com>
2018-10-25 10:57:30 +0200

    New v1, v2 and v3 management

--- src/v3/index.ts
+++ src/v3/index.ts
@@ -1,13 +1,1 @@
-import { GetElectronProcessType as GetElectronProcessTypeV2 } from '../v2/';
-
-export type ElectronProcessType = 'node' | 'browser' | 'main';
-
-export function GetElectronProcessType(): ElectronProcessType {
-    let electronProcessTypeV2 = GetElectronProcessTypeV2();
-    switch(electronProcessTypeV2) {
-        case 'renderer':
-            return 'browser';
-        default :
-            return electronProcessTypeV2 as ElectronProcessType;
-    }
-}
+export * from './electron-process-type';
